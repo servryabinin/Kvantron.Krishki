f998be70f4e90930dfee8f07684c282c89289b7c
servryabinin <servryabinin@gmail.com>
2025-11-01 14:16:10 +0300

    1

--- TabletkiForms/TestForm.cs
+++ TabletkiForms/TestForm.cs
@@ -1941,6 +1941,7 @@ namespace KrishkiForms
 
         public void GetImage(Mat img)
         {
+            #region Логирование прихода крышек
             DateTime now = DateTime.Now;
 
             if (_lastImageReceivedTime.HasValue)
@@ -1959,24 +1960,25 @@ namespace KrishkiForms
             }
 
             _lastImageReceivedTime = now;
+            #endregion
 
             if (isStreamCam)
             {
-                lock (frameLock)
+                while (_frameQueue.Count >= MaxQueueSize)
                 {
-                    latestFrame?.Dispose();
-                    latestFrame = img.Clone();
-                    newFrameAvailable = true;
+                    if (_frameQueue.TryDequeue(out var oldFrame))
+                        oldFrame.Dispose();
                 }
 
-                // Если ROI не выбран, показываем полное изображение с камеры
+                _frameQueue.Enqueue(img.Clone());
+
+                _frameAvailable.Release();
+
                 if (!LocalSettings.Instance.UseVConcat)
                 {
                     img1 = img.Clone();
-                    if (isRoiProduce == true && isROISelected == true)
-                    {
+                    if (isRoiProduce && isROISelected)
                         img1 = new Mat(img, roi);
-                    }
                     UpdatePictureBox(originPb, img1);
                 }
                 else
@@ -1984,20 +1986,16 @@ namespace KrishkiForms
                     if (!isFirstImageCam1)
                     {
                         img1 = img.Clone();
-                        if (isRoiProduce == true && isROISelected == true)
-                        {
+                        if (isRoiProduce && isROISelected)
                             img1 = new Mat(img, roi);
-                        }
                         UpdatePictureBox(originPb, img1);
                         isFirstImageCam1 = true;
                     }
                     else
                     {
                         Cv2.VConcat(img1, img.Clone(), img1);
-                        if (isRoiProduce == true && isROISelected == true)
-                        {
+                        if (isRoiProduce && isROISelected)
                             img1 = new Mat(img, roi);
-                        }
                         UpdatePictureBox(originPb, img1);
                     }
                 }
@@ -2086,25 +2084,24 @@ namespace KrishkiForms
             {
                 while (!token.IsCancellationRequested)
                 {
-                    Stopwatch stopwatch = Stopwatch.StartNew();
+
 
                     Mat frameToProcess = null;
 
                     if (isStreamCam)
                     {
-                        lock (frameLock)
-                        {
-                            if (!newFrameAvailable) continue;
-                            frameToProcess = latestFrame.Clone();
-                            newFrameAvailable = false;
-                        }
+                        await _frameAvailable.WaitAsync(token);
+                        // Забираем кадр из очереди
+                        if (!_frameQueue.TryDequeue(out frameToProcess))
+                            continue;
                     }
                     else if (isProcessingFromFolder)
                     {
-                        await Task.Delay(100);
+                        await Task.Delay(100, token);
                         lock (imageListLock)
                         {
-                            if (imageFiles.Count == 0) continue;
+                            if (imageFiles.Count == 0)
+                                continue;
 
                             try
                             {
@@ -2122,6 +2119,8 @@ namespace KrishkiForms
 
                     if (frameToProcess == null || frameToProcess.Empty())
                         continue;
+
+                    Stopwatch stopwatch = Stopwatch.StartNew();
 
                     using (frameToProcess)
                     using (Mat gray = new Mat())
@@ -2157,7 +2156,7 @@ namespace KrishkiForms
 
                         using (var timeoutCts = CancellationTokenSource.CreateLinkedTokenSource(token))
                         {
-                            timeoutCts.CancelAfter(60);
+                            timeoutCts.CancelAfter(80); // таймаут обработки одного кадра (мс)
 
                             try
                             {
@@ -2167,32 +2166,28 @@ namespace KrishkiForms
                                 var obloyTask = Task.FromResult(false);
 
                                 if (ovalityCB.Checked)
-                                {
-                                    ovalityTask = RunCheckWithTimeout(_grayForOvality, _imageForOvality, timeoutCts.Token, RunCheckOvality, capContour);
-                                }
+                                    ovalityTask = RunCheckWithTimeout(_grayForOvality, _imageForOvality,
+                                        timeoutCts.Token, RunCheckOvality, capContour);
 
                                 if (inclusionCB.Checked)
-                                {
-                                    inclusionsTask = RunCheckWithTimeout(_grayForInclusions, _imageForInclusions, timeoutCts.Token, RunCheckForInclusions, capContour);
-
-                                }
+                                    inclusionsTask = RunCheckWithTimeout(_grayForInclusions, _imageForInclusions,
+                                        timeoutCts.Token, RunCheckForInclusions, capContour);
 
                                 if (inpaintCB.Checked)
-                                {
-                                    paintTask = RunCheckWithTimeout(_grayForPaintDefects, _imageForPaintDefects, timeoutCts.Token, RunCheckForPaintDefects, capContour);
-                                }
+                                    paintTask = RunCheckWithTimeout(_grayForPaintDefects, _imageForPaintDefects,
+                                        timeoutCts.Token, RunCheckForPaintDefects, capContour);
 
                                 if (obloyCB.Checked)
-                                {
-                                    obloyTask = RunCheckWithTimeout(_grayForObloyDefects, _imageForObloyDefects, timeoutCts.Token, RunCheckForObloyDefects, capContour);
-                                }
+                                    obloyTask = RunCheckWithTimeout(_grayForObloyDefects, _imageForObloyDefects,
+                                        timeoutCts.Token, RunCheckForObloyDefects, capContour);
 
                                 await Task.WhenAll(ovalityTask, inclusionsTask, paintTask, obloyTask);
 
-                                bool anyDefect = (ovalityCB.Checked && ovalityTask.Result) ||
-                                                 (inclusionCB.Checked && inclusionsTask.Result) ||
-                                                 (inpaintCB.Checked && paintTask.Result) ||
-                                                 (obloyCB.Checked && obloyTask.Result);
+                                bool anyDefect =
+                                    (ovalityCB.Checked && ovalityTask.Result) ||
+                                    (inclusionCB.Checked && inclusionsTask.Result) ||
+                                    (inpaintCB.Checked && paintTask.Result) ||
+                                    (obloyCB.Checked && obloyTask.Result);
 
                                 if (anyDefect)
                                 {
@@ -2202,29 +2197,35 @@ namespace KrishkiForms
                                         generalDefectsCountTb.Text = blowTriggerCount.ToString();
                                     }));
                                 }
-                                PLCData.QualityStatus qualityStatus = anyDefect
+
+                                if (breakingAllowCb.Checked)
+                                {
+                                    PLCData.QualityStatus qualityStatus = anyDefect
                                     ? PLCData.QualityStatus.Bad
                                     : PLCData.QualityStatus.Good;
-                                _ = Task.Run(() => SendQualityStatus(qualityStatus), token);
+
+                                    _ = Task.Run(() => SendQualityStatus(qualityStatus), token);
+                                }
 
                                 stopwatch.Stop();
                                 UpdateTextBox(generalTime, stopwatch.ElapsedMilliseconds);
 
-                                // 🟢 лог времени обработки кадра
-                                try
-                                {
-                                    string processEntry = $"{DateTime.Now:HH:mm:ss.fff} | ProcessTime: {stopwatch.ElapsedMilliseconds} ms";
-                                    File.AppendAllText(_processTimeLogPath, processEntry + Environment.NewLine);
-                                }
-                                catch (Exception logEx)
-                                {
-                                    Debug.WriteLine($"Ошибка при записи ProcessTime лога: {logEx.Message}");
-                                }
-                                // 🔚 конец добавленного блока
+                                #region логирование времени обработки
+                                /* // 🕓 Лог времени обработки кадра
+                                 try
+                                 {
+                                     string processEntry = $"{DateTime.Now:HH:mm:ss.fff} | ProcessTime: {stopwatch.ElapsedMilliseconds} ms";
+                                     File.AppendAllText(_processTimeLogPath, processEntry + Environment.NewLine);
+                                 }
+                                 catch (Exception logEx)
+                                 {
+                                     Debug.WriteLine($"Ошибка при записи ProcessTime лога: {logEx.Message}");
+                                 }*/
+                                #endregion
                             }
                             catch (OperationCanceledException)
                             {
-                                // ОК
+                                // обработку кадра прервали — ок
                             }
                         }
                     }
@@ -2232,8 +2233,8 @@ namespace KrishkiForms
             }
             catch (Exception ex)
             {
-                BeginInvoke((Action)(() =>
-                    MessageBox.Show($"Ошибка обработки: {ex.Message}")));
+                /*BeginInvoke((Action)(() =>
+                    MessageBox.Show($"Ошибка обработки: {ex.Message}")));*/
             }
         }
 
